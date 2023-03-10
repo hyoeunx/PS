c5c2d6aab51746be2158af00e59c5e8957533ac1
hyoeunx <shimhe.06@gmail.com>
2023-03-10 23:52:35 +0900

    0310

--- Baekjoon/10810.c
+++ Baekjoon/10810.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main() {
-  int n, m, i, j, k;
+  int n, m, i, j, k, num[101];
   scanf("%d %d", &n, &m);
   for(int a=1; a<=m; a++){
     scanf("%d %d %d", &i, &j, &k);
+    for(int b=i; b<=j; b++){
+      num[a]=k;
+    }
+  }
+  for(int a=1; a<=n; a++){
+    printf("%d ", num[a]);
   }
 }
